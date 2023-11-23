61ed2a169d8adbc3ee37cf4028e7c32df2388a06
DMichavilaY <davidmichavilayan4@gmail.com>
2023-11-23 10:00:54 +0100

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public int speed;
+    public AnimationCurve curve;
+
+    void Update()
+    {
+
+        Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (inputVector.magnitude > 1)
+        {
+            inputVector.Normalize();
+        }
+
+        Vector3 movementVector = inputVector * speed * Time.deltaTime;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x + movementVector.x, -23, 23), Mathf.Clamp(transform.position.y + movementVector.z, -23, 23), 0);
+
+    }
+}
